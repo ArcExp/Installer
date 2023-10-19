c99bc3a012255883a64e54ffd63c8c8886ef3b94
ArcExp <132262106+ArcExp@users.noreply.github.com>
2023-10-19 02:30:55 +0100

    Update 2-user.sh

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -36,6 +36,9 @@ if [[ ! $AUR_HELPER == none ]]; then
   cd ~
   git clone "https://aur.archlinux.org/$AUR_HELPER.git"
   cd ~/$AUR_HELPER
+if [[ $AUR_HELPER == "paru" ]]; then
+      pacman -S --noconfirm --needed rustup
+fi
   makepkg -si --noconfirm
   # sed $INSTALL_TYPE is using install type to check for MINIMAL installation, if it's true, stop
   # stop the script and move on, not installing any more packages below that line
