96e7373a8d342bf73367e2038ee687509f1df1af
ArcExp <132262106+ArcExp@users.noreply.github.com>
2023-10-19 04:03:37 +0100

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -313,7 +313,7 @@ set_option NAME_OF_MACHINE $nameofmachine
 aurhelper () {
   # Let the user choose AUR helper from predefined list
   echo -ne "Please enter your desired AUR helper:\n"
-  options=(paru yay picaur aura trizen pacaur none)
+  options=(pacseek yay pikaur aura trizen none)
   select_option $? 4 "${options[@]}"
   aur_helper=${options[$?]}
   set_option AUR_HELPER $aur_helper
