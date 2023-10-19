5a64a4d011b8f6e60f559afad945557a3e9d9267
ArcExp <132262106+ArcExp@users.noreply.github.com>
2023-10-19 18:04:59 +0100

    Add files via upload

--- configs/hyprland-dots/.config/fish/functions/fish_greeting.fish
+++ configs/hyprland-dots/.config/fish/functions/fish_greeting.fish
@@ -1,3 +1,7 @@
 function fish_greeting
-    echo "Greetings $USER!"
+echo "Hello, $USER!"
+echo "If you need to make app windows float (such as timeshift settings) press SUPER + V"
+echo
+echo "When you're ready, you can remove this message."
+echo "To do so, go to ~/.config/fish/functions and delete 'fish_greeting.fish'"
 end
