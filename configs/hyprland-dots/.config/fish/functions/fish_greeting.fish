f53fdd5ec8720408862b291ae8150f4a127da399
ArcExp <132262106+ArcExp@users.noreply.github.com>
2023-10-22 02:04:07 +0100

    Delete configs/hyprland-dots/.config directory

--- configs/hyprland-dots/.config/fish/functions/fish_greeting.fish
+++
@@ -1,7 +0,0 @@
-function fish_greeting
-echo "Hello, $USER!"
-echo "If you need to make app windows float (such as timeshift's settings wizard) press SUPER + C"
-echo
-echo "When you're ready, you can remove this message."
-echo "To do so, go to ~/.config/fish/functions and delete 'fish_greeting.fish'"
-end
