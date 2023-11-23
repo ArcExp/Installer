5f3502cd39145b1fe8b60e637d17eefeb1d19f54
ArcExp <132262106+ArcExp@users.noreply.github.com>
2023-11-23 13:18:21 +0000

    Update 0-preinstall.sh

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -28,6 +28,7 @@ Setting up mirrors for optimal download
 source $CONFIGS_DIR/setup.conf
 iso=$(curl -4 ifconfig.co/country-iso)
 timedatectl set-ntp true
+pacman-key --init
 pacman -S --noconfirm archlinux-keyring #update keyrings to latest to prevent packages failing to install
 pacman -S --noconfirm --needed terminus-font
 setfont ter-v22b
